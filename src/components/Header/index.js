64764bdc6962380f4e7aa3d45785cdc95461a469
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-12 08:02:27 -0500

    tweak styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,15 @@ import Navigation from "../Navigation";
 
 function Header({ sections, currentSection, setCurrentSection }) {
   return (
-    <header className="flex bg-sky-700 text-white h-10 justify-between items-center">
-      <h1 className="px-2">John Doe</h1>
-      <Navigation
-        sections={sections}
-        currentSection={currentSection}
-        setCurrentSection={setCurrentSection}
-      />
+    <header className="bg-sky-700 text-white">
+      <div className="flex h-10 justify-between items-center m-auto max-w-2xl">
+        <h1 className="px-2">John Doe</h1>
+        <Navigation
+          sections={sections}
+          currentSection={currentSection}
+          setCurrentSection={setCurrentSection}
+        />
+      </div>
     </header>
   );
 }

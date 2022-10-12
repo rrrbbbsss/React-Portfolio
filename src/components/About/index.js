de6965e1baa1624d31072e6631d8e263f8b87d2b
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-12 09:05:51 -0500

    add resume section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="flex flex-col justify-center items-center">
       <img src={avatar} alt="avatar" width="100px" />
-      <p className="m-auto max-w-md">
+      <p className="m-auto max-w-md border p-2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo
         nostrum consequatur praesentium beatae. Fugit vitae quam repellendus
         quas veniam. Atque adipisci eveniet veritatis aspernatur, provident
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 function Resume() {
-  return <section>todo resume</section>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <article className="p-2 m-2">
+        <h2 className="border m-2 p-1 text-center">Resume</h2>
+        <a
+          className="hover:text-amber-300"
+          href="/resume.txt"
+          target="_blank"
+          download
+        >
+          Download my resume
+        </a>
+      </article>
+      <article className="p-2 m-2">
+        <h2 className="border m-2 p-1 text-center">Proficiencies</h2>
+        <ul className="list-disc list-inside">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>jQuery</li>
+          <li>responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequlize</li>
+          <li>MongoDB, Mongoose</li>
+        </ul>
+      </article>
+    </div>
+  );
 }
 
 export default Resume;
